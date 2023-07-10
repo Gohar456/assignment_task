7b842b5e1004010fbfe58c7018b158ee51915778
Ali Anjum <alianjum520@gmail.com>
2023-07-10 19:42:17 +0500

    cors added

--- backend_django/manufacturer/models.py
+++ backend_django/manufacturer/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from Auth.models import User
+import uuid
+
 # Create your models here.
 QUANTITY_CHOICES = (
     ('one_ton', '1 ton'),
@@ -7,11 +9,15 @@ QUANTITY_CHOICES = (
     ('three_ton', '3 ton')
 )
 
+class Transporter(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    price = models.FloatField()
+
 
 class Manufacturer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
-    #order_id = models.
     from_address = models.CharField(max_length=255)
     to_address = models.CharField(max_length=255)
     quantity = models.CharField(max_length=255, choices=QUANTITY_CHOICES)
-    
+    transporter = models.ForeignKey(Transporter, on_delete=models.CASCADE)
